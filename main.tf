6a86be8cf4051778748d0e56caeffcfebb6e1ec6
Diego Machado <130937518+diegomcap@users.noreply.github.com>
2024-09-08 04:37:16 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,12 @@
-# ARM provider block - Diego
-provider "azurerm" {
-    version = "~>2.0"
-    features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
 }
 
-# Terraform backend configuration block -pre-created
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "value"
-    storage_account_name = "value"
-    container_name       = "value"
-    key                  =  "value"
-    
-  }
+provider "azurerm" {
+  features {}
 }
